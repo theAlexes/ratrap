f3b45ec22ee7288455a39be600af013669b175f3
atax1a <>
2025-11-07 18:39:56 +0000

    stub out the blacklist library for OS X.
    FossilOrigin-Name: 0d92ce0c011b80ca4d066fc627ab8b5c4d2180770465da8a5bc9b86554bc32be

--- src/blacklist.c
+++ src/blacklist.c
@@ -1,12 +1,59 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
-void *blocklist_open(void)
+struct blacklist {};
+
+struct blacklist *
+blacklist_open(void)
 {
-    return(malloc(0));
+    void *handle = (struct blacklist *)malloc(sizeof(struct blacklist));
+    fprintf(stderr, "opening blocklist, handle %p\n", handle);
+    return handle;
 }
 
-void blocklist_close(void *it)
+void
+blacklist_close(struct blacklist *it)
 {
+    fprintf(stderr, "closing blocklist, handle %p\n", it);
     free(it);
+}
+
+int
+blacklist_sa_r(struct blacklist *handle, int action, int fd,
+               const struct sockaddr *sa, socklen_t salen, const char *msg)
+{
+    char buf[INET6_ADDRSTRLEN];
+    const char *ok = NULL;
+    fprintf(stderr, "blocklisting with handle %p: action %d, fd %d, salen %d\n", handle, action, fd, salen);
+    switch(sa->sa_family) {
+    case AF_INET:
+        {
+            const struct sockaddr_in *sin = (const struct sockaddr_in *)sa;
+            ok = inet_ntop(sa->sa_family, (void*)(&sin->sin_addr), buf, INET6_ADDRSTRLEN);
+        }
+        break;
+    case AF_INET6:
+        {
+            const struct sockaddr_in6 *sin6 = (const struct sockaddr_in6 *)sa;
+            ok = inet_ntop(sa->sa_family, (void*)(&sin6->sin6_addr), buf, INET6_ADDRSTRLEN);
+        }
+        break;
+    default:
+        fprintf(stderr, "blocklist with handle %p: unknown sa_family %d", handle, sa->sa_family);
+        errno = EINVAL;
+        return -1;
+    }
+    if(!ok) {
+        fprintf(stderr, "blocklist with handle %p: did not convert sockaddr: %s", handle, strerror(errno));
+        return -1;
+    }
+    fprintf(stderr, "sockaddr: %s\n", buf);
+    fprintf(stderr, "message: %s\n", msg);
+    return 0;
 }
